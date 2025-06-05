dff310463fae5562584b45bf122bb30de77a0e1d
Nelio Alves <acenelio@gmail.com>
2025-06-04 22:08:51 -0300

    Insert user

--- spring-boot-3-4-5/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ spring-boot-3-4-5/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.UserService;
@@ -28,5 +31,11 @@ public class UserController {
 	@GetMapping(value = "/{id}")
 	public Mono<ResponseEntity<UserDTO>> findById(@PathVariable String id) {
 		return service.findById(id).map(userDto -> ResponseEntity.ok().body(userDto));
+	}
+	
+	@PostMapping
+	public Mono<ResponseEntity<UserDTO>> insert(@RequestBody UserDTO dto, UriComponentsBuilder builder) {
+		return service.insert(dto).map(newUser -> ResponseEntity
+				.created(builder.path("/users/{id}").buildAndExpand(newUser.getId()).toUri()).body(newUser));
 	}
 }
--- spring-boot-3-4-5/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ spring-boot-3-4-5/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.workshopmongo.dto.UserDTO;
+import com.devsuperior.workshopmongo.entities.User;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
 import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 
@@ -24,5 +25,17 @@ public class UserService {
 		return repository.findById(id)
 				.map(existingUser -> new UserDTO(existingUser))
 				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
+	}
+	
+	public Mono<UserDTO> insert(UserDTO dto) {
+		User entity = new User();
+		copyDtoToEntity(dto, entity);
+		Mono<UserDTO> result = repository.save(entity).map(user -> new UserDTO(user));
+		return result;
+	}
+
+	private void copyDtoToEntity(UserDTO dto, User entity) {
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
 	}
 }
